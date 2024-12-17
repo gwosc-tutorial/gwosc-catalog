bc36e288ab8ec1157b10b23c5e84458b342750f6
Javier Roulet <jroulet@caltech.edu>
2024-12-17 12:12:29 -0500

    Linting, shortened lines to 79 characters.

--- gwosc_catalog/schema.py
+++ gwosc_catalog/schema.py
@@ -52,11 +52,13 @@ class ParameterValue:
 
     upper_error: float
         Size of the upper error-bar of the 90% credible region.
-        OK to use different uncertainty definition if noted in documentation.
+        OK to use different uncertainty definition if noted in
+        documentation.
 
     lower_error: float
         Size of the lower error-bar of the 90% credible region.
-        OK to use a different uncertainty definition if noted in documentation.
+        OK to use a different uncertainty definition if noted in
+        documentation.
 
     is_upper_bound: bool
         True if this value is an upper bound, False otherwise.
@@ -66,11 +68,13 @@ class ParameterValue:
     is_lower_bound: bool
         True if this value is an upper bound, False otherwise.
         Defaults to False if omitted.
-        Setting this to True displays a greater-than sign before the value.
+        Setting this to True displays a greater-than sign before the
+        value.
 
     decimal_places: int
-        Number of decimal places of the best value to display, must be >= 0.
-        Displayed values will be rounded to this number of decimal places.
+        Number of decimal places of the best value to display, must be
+        >= 0. Displayed values will be rounded to this number of decimal
+        places.
 
     unit: str or None
         The physical unit of the best value.
@@ -103,13 +107,15 @@ class ParameterValue:
                 )
 
         if self.parameter_name == "luminosity_distance" and self.unit != "Mpc":
-            raise ValueError("luminosity_distance parameter needs to have Mpc `unit`.")
+            raise ValueError(
+                "luminosity_distance parameter needs to have Mpc `unit`.")
 
         if self.parameter_name == "far" and self.unit != "1/year":
             raise ValueError("far parameter needs to have '1/year' `unit`.")
 
         if self.is_upper_bound and self.is_lower_bound:
-            raise ValueError("Both `is_upper_bound` and `is_lower_bound` set to True.")
+            raise ValueError(
+                "Both `is_upper_bound` and `is_lower_bound` set to True.")
 
     @classmethod
     def from_series(cls, parameter_name: str, series: pd.Series):
@@ -117,8 +123,9 @@ class ParameterValue:
         q05, median, q95 = series.quantile((0.05, 0.5, 0.95))
         error = median - q05, q95 - median
         kwargs = _condition_value_and_error(median, error)
-        return cls(
-            parameter_name=parameter_name, unit=UNITS.get(parameter_name), **kwargs
+        return cls(parameter_name=parameter_name,
+                   unit=UNITS.get(parameter_name),
+                   **kwargs
         )
 
 
@@ -138,7 +145,8 @@ class Link:
     description: str
         A brief description of the resource.
 
-    For a link to posterior samples, use ``ParameterSet.data_url`` instead.
+    For a link to posterior samples, use ``ParameterSet.data_url``
+    instead.
     """
 
     url: str
@@ -182,8 +190,9 @@ class ParameterSet:
         The pipeline name used to generate the parameter estimations.
 
     data_url: str, url
-        This should point to the source data for the analysis of each event.
-        It typically points to a posterior sample file if available.
+        This should point to the source data for the analysis of each
+        event. It typically points to a posterior sample file if
+        available.
 
     waveform_family: str
         The waveform approximant used for this parameter set.
@@ -209,18 +218,19 @@ class ParameterSet:
 
     @classmethod
     def from_samples(
-        cls,
-        samples: pd.DataFrame,
-        pe_set_name,
-        data_url,
-        waveform_family,
-        is_preferred,
-        links=None,
-    ):
+            cls,
+            samples: pd.DataFrame,
+            pe_set_name,
+            data_url,
+            waveform_family,
+            is_preferred,
+            links=None,
+        ):
         """
         Constructor from a ``pandas.DataFrame`` of posterior samples.
         """
-        parameters = [ParameterValue.from_series(*item) for item in samples.items()]
+        parameters = [ParameterValue.from_series(*item)
+                      for item in samples.items()]
         return cls(
             pe_set_name=pe_set_name,
             data_url=data_url,
@@ -257,7 +267,8 @@ class Event:
 
     gracedb_id: str or None
         The associated GraceDB superevent ID if any.
-        This is an optional key but we recommend adding it whenever possible.
+        This is an optional key but we recommend adding it whenever
+        possible.
 
     detectors: list[str]
         A list of detectors for which strain data should be publicly
@@ -283,25 +294,30 @@ class Event:
         # Event name should have format GWYYMMDD_HHMMSS
         if not bool(re.match(r"^GW\d{6}_\d{6}$", self.event_name)):
             raise ValueError("Event name should have format GWYYMMDD_HHMMSS")
+
         # Detectors validation
         if self.detectors is not None:
             for detector in self.detectors:
                 if detector not in ["H1", "L1", "V1", "K1", "G1"]:
-                    raise ValueError(f"Unrecognized detector short name: {detector}")
+                    raise ValueError(
+                        f"Unrecognized detector short name: {detector}")
+
         # Events cannot have empty search list
         if len(self.search) == 0:
             raise ValueError("Search list is empty.")
+
         # Events can have empty PE sets
         if self.pe_sets is None:
             return
         elif len(self.pe_sets) == 0:
             return
+
         # If events have PE sets, exactly one should be preferred
-        n_preferred = np.count_nonzero([pe_set.is_preferred for pe_set in self.pe_sets])
+        n_preferred = np.count_nonzero(
+            [pe_set.is_preferred for pe_set in self.pe_sets])
         if n_preferred != 1:
-            raise ValueError(
-                f"Exactly one of the `pe_sets` should be preferred, got {n_preferred}."
-            )
+            raise ValueError("Exactly one of the `pe_sets` should be "
+                             f"preferred, got {n_preferred}.")
 
     @classmethod
     def from_json(cls, event: dict):
@@ -313,7 +329,8 @@ class Event:
             **e,
             search=[SearchResult.from_json(s) for s in searches],
             # Make a list of ParameterSets only if it's non-empty
-            pe_sets=[ParameterSet.from_json(pe_set) for pe_set in pe_sets] or None,
+            pe_sets=[ParameterSet.from_json(pe_set)
+                     for pe_set in pe_sets] or None,
         )
 
 
@@ -330,7 +347,8 @@ class Catalog:
         The name of the catalog.
 
     release_date: str
-        The date of the public release of the data in the format YYYY-MM-DD.
+        The date of the public release of the data in the format
+        YYYY-MM-DD.
 
     catalog_description: str
         A description of the catalog.
@@ -358,7 +376,8 @@ class Catalog:
             raise ValueError("Catalog release_date error.")
         if self.schema_version != __version__:
             warnings.warn(
-                f"Schema version provided {self.schema_version} is different from current: {__version__}",
+                f"Schema version provided {self.schema_version} is different "
+                f"from current: {__version__}",
                 UserWarning,
                 stacklevel=1,
             )
@@ -372,7 +391,8 @@ class Catalog:
     def from_json(cls, catalog):
         c = catalog.copy()
         events = c.pop("events")
-        return Catalog(**c, events=[Event.from_json(event) for event in events])
+        return Catalog(**c,
+                       events=[Event.from_json(event) for event in events])
 
 
 def _condition_value_and_error(value, error) -> dict:
@@ -428,7 +448,8 @@ def _set_logger():
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(name)s: %(message)s")
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(levelname)s] %(name)s: %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
@@ -445,7 +466,8 @@ def main():
 
     parser = argparse.ArgumentParser(
         prog="validateschema",
-        description="Validate the upload json schema for a GWOSC community catalog.",
+        description="""
+            Validate the upload json schema for a GWOSC community catalog.""",
     )
     parser.add_argument("filename", help="Json file to check")
     args = parser.parse_args()
